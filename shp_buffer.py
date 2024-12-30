aa05e30d1c660e08bd50d2e412d055ded84a0a86
Janak Parajuli <janak.parajuli1@gmail.com>
2024-12-30 16:47:18 -0500

    250 buffer created ... <150 remaining

--- shp_buffer.py
+++ shp_buffer.py
@@ -5,7 +5,7 @@ import os
 
 # Path to the input point shapefile
 folder = r"E:\UAH_Classes\Research\Kansas"  # Replace with your directory path
-filename = "\PWS\kansasPWS.shp"  # Replace with your shapefile name
+filename = r"PWS\kansasPWS.shp"  # Replace with your shapefile name
 input_path = os.path.join(folder, filename)
 
 # Path to the output buffer shapefile
@@ -18,11 +18,14 @@ points_gdf = gpd.read_file(input_path)
 
 # Specify the buffer distance in the units of the shapefile's coordinate system
 buffer_distance = 250  # Change this value based on your specific requirements
+print(points_gdf.crs)  # print crs of point data
 
 # Create a buffer around each point
+print(f"Creating buffer of {buffer_distance}")
 points_gdf['geometry'] = points_gdf.buffer(buffer_distance)
 
 # Save the buffered geometry to a new shapefile
+print(f"Saving buffer of {buffer_distance}")
 points_gdf.to_file(output_path)
 
 print(f"Buffered shapefile saved to: {output_path}")
