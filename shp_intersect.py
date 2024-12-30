4430076a6adca46ecf76f27378fe6d677f057be7
Janak Parajuli <janak.parajuli1@gmail.com>
2024-12-30 13:10:15 -0500

    code added

--- shp_intersect.py
+++ shp_intersect.py
@@ -1,2 +1,16 @@
 # Python code to extract intersection of shp files
+import geopandas as gpd
+import os
+
+# Read shapefiles
+folder = r"E:\UAH_Classes\Research\Kansas\"
+clip_file = os.path.join(folder + r"Boundary\KansasCity_MO_UA_mer.shp")
+shp_file1 = os.path.join(folder + r"Buildings\Heights\3D-GloBFP\United_States_of_America\Kansas.shp")
+shp_file2 = os.path.join(folder + r"Buildings\Heights\3D-GloBFP\United_States_of_America\Missouri.shp")
+
+# Intersect three shapefiles
+glo3DBuildings = gpd.overlay(clip_file, shp_file1, shp_file2, how = 'intersection')
+
+glo3DBuildings.to_file(os.path.join(folder + r"Buildings\Heights\glo3DBuildings.shp"))
+
 
