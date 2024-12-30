c816764eaab65e7808503e95ece5a254be0dd1b9
Janak Parajuli <janak.parajuli1@gmail.com>
2024-12-30 15:02:22 -0500

    intersecting code added

--- shp_reproject.py
+++ shp_reproject.py
@@ -7,7 +7,7 @@ import os
 folder = r"E:\UAH_Classes\Research\Kansas"
 print(f"Reading files ...")
 bound_file = os.path.join(folder, r"Boundary\KansasCity_MO_UA_mer.shp")
-shp_file = os.path.join(folder, r"Buildings\Heights\KM_glo3DBuildings.shp")
+shp_file = os.path.join(folder, r"Buildings\Heights\KM_glo3DBuildings_Mer.shp")
 
 bound_file = gpd.read_file(bound_file)
 shp_file = gpd.read_file(shp_file)
@@ -21,15 +21,16 @@ if shp_file.crs != bound_file.crs:
 else:
     print("CRS matches for both files")
 
+print(bound_file.crs)
 print(shp_file.crs)
 print("All set for clipping now")
 
 # Intersect both shapefiles
-# print(f"Intersecting ...")
-# # glo3DBuildings = gpd.overlay(clip_file, merged_shp_file, how = 'intersection')
+print(f"Intersecting ...")
+glo3DBuildings = gpd.overlay(bound_file, shp_file, how = 'intersection')
 
 print(f"Saving ...")
 #glo3DBuildings.to_file(os.path.join(folder, "Buildings","Heights","glo3DBuildings.shp"))
-shp_file.to_file(os.path.join(folder, "Buildings","Heights","KM_glo3DBuildings_Mer.shp"))
+glo3DBuildings.to_file(os.path.join(folder, "Buildings","Heights","glo3DBuildings.shp"))
 print("Operation Completed ...")
 
