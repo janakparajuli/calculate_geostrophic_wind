a39a073dbc1282cd4ea721e001b1a338d8697e8a
Janak Parajuli <janak.parajuli1@gmail.com>
2024-12-30 17:14:00 -0500

    intersecting 3d buildings with pws buffer 250

--- shp_reproject.py
+++ shp_reproject.py
@@ -6,31 +6,31 @@ import os
 # Read shapefiles
 folder = r"E:\UAH_Classes\Research\Kansas"
 print(f"Reading files ...")
-bound_file = os.path.join(folder, r"Boundary\KansasCity_MO_UA_mer.shp")
-shp_file = os.path.join(folder, r"Buildings\Heights\KM_glo3DBuildings_Mer.shp")
+clip_file = os.path.join(folder, r"PWS\PWSBuffer250.shp")
+shp_file = os.path.join(folder, r"Buildings\Heights\glo3DBuildings.shp")
 
-bound_file = gpd.read_file(bound_file)
+clip_file = gpd.read_file(clip_file)
 shp_file = gpd.read_file(shp_file)
 
 # Ensure the CRS matches for all GeoDataFrames before overlay
 print("Comparing CRS")
-if shp_file.crs != bound_file.crs:
+if shp_file.crs != clip_file.crs:
     print("CRS mismatch detected, aligning CRS...")
-    shp_file = shp_file.to_crs(bound_file.crs)
+    shp_file = shp_file.to_crs(clip_file.crs)
 
 else:
     print("CRS matches for both files")
 
-print(bound_file.crs)
+print(clip_file.crs)
 print(shp_file.crs)
 print("All set for clipping now")
 
 # Intersect both shapefiles
 print(f"Intersecting ...")
-glo3DBuildings = gpd.overlay(bound_file, shp_file, how = 'intersection', keep_geom_type=False)
+glo3DBuildings_PWS250 = gpd.overlay(clip_file, shp_file, how = 'intersection', keep_geom_type=False)
 
 print(f"Saving ...")
 #glo3DBuildings.to_file(os.path.join(folder, "Buildings","Heights","glo3DBuildings.shp"))
-glo3DBuildings.to_file(os.path.join(folder, "Buildings","Heights","glo3DBuildings.shp"))
+glo3DBuildings_PWS250.to_file(os.path.join(folder, "Buildings","Heights","glo3DBuildings_PWS250.shp"))
 print("Operation Completed ...")
 
