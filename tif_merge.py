a2e07a38d975f8fb1662e78a25df504366c9a755
Janak Parajuli <janak.parajuli1@gmail.com>
2025-01-01 16:41:07 -0500

    max function defined

--- tif_merge.py
+++ tif_merge.py
@@ -1,2 +1,19 @@
 # This code merges tiff files with same extent into one tiff
 
+# Define function to find max reflectance values
+import numpy as np
+
+
+def max_merge_method(old_data, new_data, old_nodata, new_nodata, index=None):
+    """
+    Custom merge function to take the maximum value where datasets overlap.
+    Assumes that the nodata values are marked identically in both datasets.
+    """
+    # Create a mask where either the old data or new data is not nodata
+    mask = (old_data != old_nodata) & (new_data != new_nodata)
+
+    # Use maximum value from old and new data where mask is True
+    max_data = np.maximum(old_data, new_data)
+
+    # Where the mask is False, keep the old data values (preserve existing data)
+    return np.where(mask, max_data, old_data)
