b058047ca21399f5dcd6951e0c72003665d4a867
Janak Parajuli <janak.parajuli1@gmail.com>
2025-01-02 11:57:54 -0500

    instructions added

--- plot_footprints_canopies_comparison.py
+++ plot_footprints_canopies_comparison.py
@@ -8,11 +8,12 @@ data_buildings = {
     'Mean Building Height (m)': [9.127, 9.084, 9.009, 9.025, 9.008, 9.018, 9.007],
     'Coverage (%)': [13.49, 11.91, 11.62, 11.49, 11.18, 10.76, 10.40]
 }
-
+# Create buildings dataframe
 df_buildings = pd.DataFrame(data_buildings)
 # Convert PWS to a numeric value for easier plotting
 df_buildings['PWS_Value'] = df_buildings['PWS'].str.extract(r'(\d+)').astype(int)
 
+# Create trees dataframe
 data_canopy = {
     "PWS": ["PWS20", "PWS40", "PWS60", "PWS80", "PWS100", "PWS150", "PWS200"],
     "Total Canopy Area (sq. m)": [294183.0, 1192810.0, 3057278.0, 5460120.0, 8534680.0, 18985030.0, 33199458.0],
